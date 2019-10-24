92b8f5d0fc7319793ac9f57535829423666e80c3
Arty Stable <48605866+artystable@users.noreply.github.com>
2019-10-24 10:40:28 -0400

    add test function

---
+++ GitHubHeaderBrushStroke.js
@@ -0,0 +1,12 @@
+/**
+ * Summary: Change GitHub header to custom color.
+ * Description: This function takes a CSS color property value as input and applies it to the header element at https://github.com.
+ * 
+ * @link https://github.com
+ * @param {string}   [color='#24292e'] CSS input color that will be assigned to GitHub header element after function run.
+ * @return {null} Returns null after function run.
+ */
+function GitHubHeaderBrushStroke(color = '#279f43') {
+    document.querySelector('header').style.cssText = `background-color: ${color};`;
+    return null;
+}
